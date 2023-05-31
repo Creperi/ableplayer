1c7de73a18e051e44700e63bfca8c54d70303a73
Elias Poniridis <68995203+Creperi@users.noreply.github.com>
2023-05-31 23:47:51 +0300

    Create gr.js
    I added greek language support

---
+++ translations/gr.js
@@ -0,0 +1,355 @@
+{
+
+"audio": "ήχος",
+
+"video": "βίντεο",
+
+"playerHeading": "αναπαραγωγή περιεχομένου",
+
+"faster": "γρήγορα",
+
+"slower": "αργά",
+
+"play": "παίξε",
+
+"pause": "παύση",
+
+"stop": "σταμάτα",
+
+"restart": "επανεκκίνηση",
+
+"prevChapter": "προηγούμενο κεφάλαιο",
+
+"nextChapter": "επόμενο κεφάλαιο",
+
+"prevTrack": "προηγούμενο κομμάτι",
+
+"nextTrack": "επόμενο κομμάτι",
+
+"rewind": "πίσω",
+
+"forward": "μπροστά",
+
+"captions": "υπότιτλοι",
+
+"showCaptions": "δείξε υπότιτλους",
+
+"hideCaptions": "κρύψε υπότιτλους",
+
+"captionsOff": "υπότιτλοι απενεργοποιημένοι",
+
+"showTranscript": "δείξε αντίγραφο",
+
+"hideTranscript": "κρύψε αντίγραφο",
+
+"turnOnDescriptions": "ενεργοποίησε τις περιγραφές",
+
+"turnOffDescriptions": "απενεργοποίησε τις περιγραφές",
+
+"chapters": "κεφάλαια",
+
+"newChapter": "νέο κεφάλαιο",
+
+"language": "γλώσσα",
+
+"sign": "νοηματική γλώσσα",
+
+"showSign": "δείξε νοηματική γλώσσα",
+
+"hideSign": "κρύψε νοηματική γλώσσα",
+
+"seekbarLabel": "χρονοδιάγραμμα",
+
+"mute": "σίγαση",
+
+"unmute": "κατάργηση σίγασης",
+
+"volume": "ένταση",
+
+"volumeHelp": "πάτα για να μπεις στο ρυθμστικό έντασης",
+
+"volumeUpDown": "ένταση πάνω κάτω",
+
+"volumeSliderClosed": "ρυθμιστικό έντασης κλειστό",
+
+"preferences": "επιλογές",
+
+"enterFullscreen": "πρόσβαση στην πλήρη οθόνη",
+
+"exitFullscreen": "έξοδος από την πλήρη οθόνη",
+
+"fullscreen": "πλήρη οθόνη",
+
+"speed": "ταχύτητα",
+
+"and": "και",
+
+"or": "ή",
+
+"spacebar": "μπάρα πληκτρολογίου",
+
+"transcriptTitle": "αντίγραφο",
+
+"lyricsTitle": "στίχοι",
+
+"autoScroll": "αυτόματη κύλιση",
+
+"unknown": "άγνωστο",
+
+"statusPlaying": "παίζει",
+
+"statusPaused": "βρίσκεται σε παύση",
+
+"statusStopped": "σταματημένο",
+
+"statusWaiting": "βρίσκεται σε αναμονή",
+
+"statusBuffering": "σε προσωρινή αποθήκευση",
+
+"statusUsingDesc": "χρήση της περιγραφόμενης έκδοσης",
+
+"statusLoadingDesc": "φόρτωση της περιγραφόμενης έκδοσης",
+
+"statusUsingNoDesc": "χρήση της μη περιγραφόμενης έκδοσης",
+
+"statusLoadingNoDesc": "φόρτωση της μη περιγραφόμενης έκδοσης",
+
+"statusLoadingNext": "φόρτωση επόμενου κομματιού",
+
+"statusEnd": "τέλος κομματιού",
+
+"selectedTrack": "επιλεγμένο κομμάτι",
+
+"alertDescribedVersion": "χρήση της περιγραφόμενης έκδοσης του βίντεο",
+
+"alertNonDescribedVersion": "χρήση της μη περιγραφόμενης έκδοσης του βίντεο",
+
+"fallbackError1": "΄συγγνώμη, ο φυλλομετρητής δεν μπορεί να το τρέξει",
+
+"fallbackError2": "οι ακόλουθοι φυλλομετρητές είναι γνωστοί ότι λειτουργούν με αυτό το πρόγραμμα αναπαραγωγής πολυμέσων",
+
+"orHigher": "η ψηλότερα",
+
+"prefMenuCaptions": "υπότιτλοι",
+
+"prefMenuDescriptions": "περιγραφές",
+
+"prefMenuKeyboard": "πληκτρολόγιο",
+
+"prefMenuTranscript": "αντίγραφο",
+
+"prefTitleCaptions": "επιλογές υπότιτλων",
+
+"prefTitleDescriptions": "επιλογές περιγραφής ήχου",
+
+"prefTitleKeyboard": "επιλογές πληκτρολογίου",
+
+"prefTitleTranscript": "επιλογές αντιγραφής",
+
+"prefIntroCaptions": "οι παρακάτω προτιμήσεις ελέγχουν τον τρόπο εμφάνισης των υπότιτλων.",
+
+"prefIntroDescription1": "αυτό το πρόγραμμα αναπαραγωγής πολυμέσων υποστηρίζει ηχητική περιγραφή με δύο τρόπους: ",
+
+"prefIntroDescription2": "το παρόν βίντεο έχει ",
+
+"prefIntroDescriptionNone": "το παρόν βίντεο δεν έχει περιγραφή ήχου σε οποιαδήποτε μορφή.",
+
+"prefIntroDescription3": "χρησιμοποιήστε την παρακάτω φόρμα για να ορίσετε τις προτιμήσεις σας σχετικά με την ηχητική περιγραφή που βασίζεται σε κείμενο.",
+
+"prefIntroDescription4": "αφού αποθηκεύσετε τις ρυθμίσεις σας, η ηχητική περιγραφή μπορεί να ενεργοποιηθεί/απενεργοποιηθεί χρησιμοποιώντας το κουμπί Περιγραφή.",
+
+"prefIntroKeyboard1": "το πρόγραμμα αναπαραγωγής πολυμέσων σε αυτήν την ιστοσελίδα μπορεί να λειτουργήσει από οπουδήποτε στη σελίδα χρησιμοποιώντας συντομεύσεις πληκτρολογίου (δείτε παρακάτω για μια λίστα).",
+
+"prefIntroKeyboard2": "τα πλήκτρα τροποποίησης (Shift, Alt και Control) μπορούν να αντιστοιχιστούν παρακάτω.",
+
+"prefIntroKeyboard3": "ΣΗΜΕΙΩΣΗ: Ορισμένοι συνδυασμοί πλήκτρων ενδέχεται να έρχονται σε διένεξη με τα κλειδιά που χρησιμοποιούνται από το πρόγραμμα περιήγησής σας ή/και άλλες εφαρμογές λογισμικού. Δοκιμάστε διάφορους συνδυασμούς πλήκτρων τροποποίησης για να βρείτε αυτό που σας ταιριάζει.",
+
+"prefIntroTranscript": "οι ακόλουθες προτιμήσεις επηρεάζουν τη διαδραστική μεταγραφή.",
+
+"prefCookieWarning": "η αποθήκευση των προτιμήσεών σας απαιτεί cookies.",
+
+"prefHeadingKeyboard1": "Πλήκτρα τροποποίησης που χρησιμοποιούνται για συντομεύσεις",
+
+"prefHeadingKeyboard2": "Τρέχουσες συντομεύσεις πληκτρολογίου",
+
+"prefHeadingDescription": "περιγραφή ήχου",
+
+"prefHeadingTextDescription": "περιγραφή ήχου βασισμένη στο κείμενο",
+
+"prefHeadingCaptions": "υπότιτλοι",
+
+"prefHeadingTranscript": "διαδραστικό αντίγραφο",
+
+"prefAltKey": "Alt",
+
+"prefCtrlKey": "Control",
+
+"prefShiftKey": "Shift",
+
+"escapeKey": "Escape",
+
+"escapeKeyFunction": "κλείσε το τρέχον παράθυρο διαλόγου ή το αναδυόμενο μενού",
+
+"prefDescFormat": "προεπιλεγμένη μορφή",
+
+"prefDescFormatHelp": "αν και οι δύο μορφές είναι διαθέσιμες, θα χρησιμοποιηθεί μόνο μία.",
+
+"prefDescFormatOption1": "εναλλακτική περιγραφόμενη έκδοση του βίντεο",
+
+"prefDescFormatOption1b": "μια εναλλακτική περιγραφόμενη έκδοση",
+
+"prefDescFormatOption2": "περιγραφή βάσει κειμένου, που ανακοινώθηκε από το πρόγραμμα ανάγνωσης οθόνης",
+
+"prefDescFormatOption2b": "περιγραφή με βάση το κείμενο",
+
+"prefDescPause": "αυτόματη παύση του βίντεο όταν ξεκινά η περιγραφή",
+
+"prefDescVisible": "κάντε την περιγραφή ορατή",
+
+"prefDescVoice": "φωνή",
+
+"prefDescRate": "ρυθμός",
+
+"prefDescPitch": "Pitch",
+
+"prefDescPitch1": "αρκετά χαμηλή",
+
+"prefDescPitch2": "χαμηλή",
+
+"prefDescPitch3": "κανονική",
+
+"prefDescPitch4": "υψηλή",
+
+"prefDescPitch5": "αρκετά υψηλή",
+
+"sampleDescriptionText": "Adjust settings to hear this sample text.",
+
+"prefHighlight": "Highlight transcript as media plays",
+
+"prefTabbable": "Keyboard-enable transcript",
+
+"prefCaptionsFont": "γραμματοσειρά",
+
+"prefCaptionsColor": "χρώμα κειμένου",
+
+"prefCaptionsBGColor": "φόντο",
+
+"prefCaptionsSize": "μέγεθος γραμματοσειράς",
+
+"prefCaptionsOpacity": "αδιαφάνεια",
+
+"prefCaptionsStyle": "στιλ",
+
+"serif": "serif",
+
+"sans": "sans-serif",
+
+"cursive": "cursive",
+
+"fantasy": "fantasy",
+
+"monospace": "monospace",
+
+"white": "άσπρο",
+
+"yellow": "κίτρινο",
+
+"green": "πράσινο",
+
+"cyan": "σιάν",
+
+"blue": "μπλε",
+
+"magenta": "magenta",
+
+"red": "κόκκινο",
+
+"black": "μαύρο",
+
+"transparent": "διαφανές",
+
+"solid": "στερεό",
+
+"captionsStylePopOn": "Pop-on",
+
+"captionsStyleRollUp": "Roll-up",
+
+"prefCaptionsPosition": "θέση",
+
+"captionsPositionOverlay": "επικάλυμμα",
+
+"captionsPositionBelow": "κάτω από το βίντεο",
+
+"sampleCaptionText": "Sample caption text",
+
+"prefSuccess": "οι αλλαγές έχουν αποθηκευτεί.",
+
+"prefNoChange": "δεν έχει πραγματοποιηθεί καμία αλλαγή.",
+
+"help": "βοήθεια",
+
+"helpTitle": "βοήθεια",
+
+"save": "αποθήκευση",
+
+"cancel": "άκυρο",
+
+"ok": "οκ",
+
+"done": "Done",
+
+"closeButtonLabel": "κλείσε παράθυρο διαλόγου",
+
+"windowButtonLabel": "επιλογές παραθύρου",
+
+"windowMove": "μετακίνησε",
+
+"windowMoveAlert": "τράβα ή χρησιμοποίησε τα πλήκτρα βέλους για την μετακίνηση του παραθύρου. Πάτα enter για να σταματήσεις",
+
+"windowResize": "αλλαγή μεγέθους",
+
+"windowResizeHeading": "αλλαγή μεγέθους παραθύρου",
+
+"windowResizeAlert": "το παράθυρο έχει αλλάξει μέγεθος.",
+
+"windowClose": "κλείσε",
+
+"width": "μήκος",
+
+"height": "ύψος",
+
+"windowSendBack": "Send to back",
+
+"windowSendBackAlert": "Αυτό το παράθυρο είναι δίπλα από άλλα αντικείμενα στην σελίδα.",
+
+"windowBringTop": "Φέρτε το παράθυρο μπροστά",
+
+"windowBringTopAlert": "Αυτό το παράθυρο βρίσκεται τώρα μπροστά από άλλα αντικείμενα στην οθόνη.",
+
+"resultsSummary1": "αναζητήσατε για:",
+
+"resultsSummary2": "βρέθηκε",
+
+"resultsSummary3": "Ταιριάζοντα αντικείμενα.",
+
+"resultsSummary4": "Πατήστε στον χρόνο που σχετίζεται με οποιοδήποτε αντικείμενο για να παίξετε το βίντεο από εκείνο το σημείο",
+
+"noResultsFound": "Δεν βρέθηκαν αποτελέσματα.",
+
+"searchButtonLabel": "παίξε στο",
+
+"hour": "ώρα",
+
+"minute": "λεπτό",
+
+"second": "δευτερόλεπτα",
+
+"hours": "ώρες",
+
+"minutes": "λεπτά",
+
+"seconds": "δευτερόλεπτα",
+
+"enableKeyboardShortcuts": "ενεργοποίηση συντομεύσεων πληκτρολογίου"
+
+}
